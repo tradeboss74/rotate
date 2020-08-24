a5290dd681ff6ab29c5dbf02944beff492da203d
tradeboss74 <stryzhak@gmail.com>
2020-08-24 22:50:01 +0300

    create background foto, and make some changes in /app, but has a mistakes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import { Banner } from './components';
 
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
 export default App;
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import style from './styles.module.scss';
 
+const Banner= () => {
+    return 2+2
+};
 
+export default Banner;
